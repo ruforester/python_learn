34fd0389b7cd3035d569b838d687e3b41ff90c61
ruforester <ruforester@gmail.com>
2023-11-12 16:43:26 +0200

    d31 comlete

--- weather-api/main.py
+++ weather-api/main.py
@@ -1,13 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
+station_list = pd.read_csv("weather-api/data-small/stations.txt", skiprows=17).to_html()
+
+@app.route("/")
+def home():
+    return render_template("home.html", data=station_list)
+
+
 @app.route("/api/v1/<station>/<date>")
 def api(station, date):
-    temperature = 23
+    filename = f"weather-api/data-small/TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze()/10
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+@app.route("/api/v1/<station>")
+def station_all_dates(station):
+    filename = f"weather-api/data-small/TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df[['STAID', '    DATE', '   TG']].to_dict(orient='records')
+    return result
 
 
 if __name__ == "__main__":
